fbb03666e6fa7d1183de0505d914669839cbcf22
Ben Dzaebel <bdzaebel@inovex.de>
2022-07-20 08:17:33 +0200

    smooth turning

--- src/main.rs
+++ src/main.rs
@@ -165,7 +165,9 @@ fn player_movement_system(
     mut input_event: EventReader<InputEvent>,
     mut query: Query<(&mut Transform, &MovementSpeed, &mut PlayerState), With<Player>>,
     time: Res<Time>,
+    mut target_rot: Local<Quat>,
 ) {
+    let turn_speed: f32 = 20.0;
     for (mut transform, speed, mut state) in query.iter_mut() {
         let mut direction = Vec3::default();
         for event in input_event.iter() {
@@ -180,18 +182,25 @@ fn player_movement_system(
         if direction.length() > 0.0 {
             let normalized_dir = direction.normalize();
             transform.translation += normalized_dir * speed.0 * time.delta_seconds();
+
+            // Rotation
             let angle = normalized_dir.angle_between(Vec3::new(0.0, 0.0, 1.0));
-            transform.rotation = Quat::from_axis_angle(
-                Vec3::new(0.0, 1.0, 0.0),
-                if normalized_dir.x > 0.0 {
-                    angle
-                } else {
-                    -angle
-                },
-            );
+            *target_rot = Quat::from_rotation_y(if normalized_dir.x > 0.0 {
+                angle
+            } else {
+                -angle
+            });
             state.state = PlayerStateEnum::MOVING;
         } else {
             state.state = PlayerStateEnum::IDLE;
+        }
+
+        let angle_to_target = transform.rotation.angle_between(*target_rot);
+        if angle_to_target > 0.0 {
+            let t = turn_speed / angle_to_target;
+            transform.rotation = transform
+                .rotation
+                .slerp(*target_rot, 1.0_f32.min(t * time.delta_seconds()));
         }
     }
 }
@@ -234,15 +243,18 @@ fn camera_follow_player_system(
     mut camera_query: Query<&mut Transform, With<Camera>>,
     player_query: Query<&Transform, (With<Player>, Without<Camera>)>,
     mut inital_position: Local<Option<Vec3>>,
+    time: Res<Time>,
 ) {
     if let Ok(mut camera_transform) = camera_query.get_single_mut() {
         if inital_position.is_none() {
             *inital_position = Some(camera_transform.translation);
         }
         if let Ok(player_transform) = player_query.get_single() {
-            
             let pt = player_transform.translation;
-            camera_transform.translation = (*inital_position).unwrap() + Vec3::new(pt.x, 0.0, pt.z);
+            let target_pos = (*inital_position).unwrap() + Vec3::new(pt.x, 0.0, pt.z);
+            camera_transform.translation = camera_transform
+                .translation
+                .lerp(target_pos, 7.0 * time.delta_seconds())
         }
     }
 }
